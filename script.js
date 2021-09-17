5e4e9cc871c7005f15a6c8ceddd3a478ba1819a7
vytcka4 <vytautas.freelance@gmail.com>
2021-09-17 21:57:18 +0300

    commit

--- script.js
+++ script.js
@@ -95,6 +95,25 @@ nav.addEventListener('mouseout', function (e) {
   handleHover(e, 1);
 });
 
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 // const header = document.querySelector('.header');
 
 // const allSections = document.querySelectorAll('.section');
